7341de8d831f1e69ec12e83368b1896085703f14
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-01-30 23:08:08 +0100

    Attempt to fix Windows build

--- src/Utils.h
+++ src/Utils.h
@@ -6,27 +6,27 @@
 //TODO: Not inline these
 //Call this once, before ever calling one of these `random()` functions
 inline void randomInit() {
-	srandom(time(nullptr));
+	srand(time(nullptr));
 }
 
 inline float random(const float max)
 {
-	return static_cast<float>(random()) / static_cast<float>(RAND_MAX) * max;
+	return static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * max;
 }
 
 inline float random(const float min, const float max)
 {
-	return static_cast<float>(random()) / static_cast<float>(RAND_MAX) * (max - min) + min;
+	return static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * (max - min) + min;
 }
 
 inline long random(const int max)
 {
-	return random() % max;
+	return rand() % max;
 }
 
 inline long random(const int min, const int max)
 {
-	return random() % (max - min) + min;
+	return rand() % (max - min) + min;
 }
 
 #endif //FIREWORKSPGE_UTILS_H
